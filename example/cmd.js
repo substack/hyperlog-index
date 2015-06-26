a6a033c3dc5f9a6ec9c48202a29253ececb499f0
James Halliday <mail@substack.net>
2015-06-25 21:29:09 -0700

    desired hypothetical usage

--- example/cmd.js
+++ example/cmd.js
@@ -1,68 +1,24 @@
 var level = require('level');
 var through = require('through2');
 
-var db = level('/tmp/log.db');
-var idb = level('/tmp/index.db');
+var hdb = level('/tmp/log.db');
+var idb = level('/tmp/index.db', { valueEncoding: 'json' });
 
 var hyperlog = require('hyperlog');
-var log = hyperlog(db);
-
-//var indexer = require('../');
-//var ix = indexer(log, idb);
+var log = hyperlog(hdb);
 
 var sub = require('subleveldown');
-var sum = sub(idb, 'sum');
+var indexer = require('../');
 
-sum.get('change', function (err, ch) {
-    if (err && err.type === 'NotFoundError') {
-        ch = 0; // initial value
-    }
-    else if (err) return console.error(err.stack);
-    else ch = Number(ch);
-console.log('ch=', ch, typeof ch); 
-    
-    var s = log.createReadStream({ since: ch });
-    s.pipe(through.obj(write, end));
-    s.on('error', function (err) {
-        console.log('err=', err);
+indexer(log, sub(idb, 'sub'), function (row, db, next) {
+    db.get('state', function (err, value) {
+        if (process.argv[2] === 'put') {
+            var n = Number(process.argv[3]);
+            db.put('state', (value || 0) + n, next);
+        }
+        else {
+            console.log(value || 0);
+            next();
+        }
     });
-    
-    function write (row, enc, next) {
-console.log('ROW=', row); 
-        var doc = JSON.parse(row.value);
-console.log('DOC=', doc); 
-        sum.get('state', function (err, value) {
-console.log('VALUE=', value); 
-            var value = JSON.parse(value || '0');
-            var nvalue = JSON.stringify(value + doc.n);
-            sum.batch([
-                {
-                    type: 'put',
-                    key: 'state',
-                    value: nvalue
-                },
-                {
-                    type: 'put',
-                    key: 'change',
-                    value: String(row.change)
-                }
-            ], next);
-        });
-    }
-    
-    function end () {
-        sum.get('state', function (err, value) {
-            console.log('value=', value);
-        });
-    }
 });
-
-if (process.argv[2] === 'add') {
-    var doc = JSON.stringify({ n: Number(process.argv[2]) });
-    log.append(doc, function (err) {
-        //if (err) return console.error(err.stack);
-    });
-}
-else if (process.argv[2] === 'get') {
-    // ugh...
-}
