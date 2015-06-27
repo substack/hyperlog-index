6c494892c12b983a99c1285bada68c284f3ed53a
James Halliday <mail@substack.net>
2015-06-26 18:18:30 -0700

    prior test from empty indexes

---
+++ test/prior.js
@@ -0,0 +1,31 @@
+var memdb = require('memdb');
+var indexer = require('../');
+var test = require('tape');
+
+test('resume from prior state', function (t) {
+    t.plan(2);
+    var hdb = memdb();
+    var idb = memdb({ valueEncoding: 'json' });
+    
+    var hyperlog = require('hyperlog');
+    var log = hyperlog(hdb, { valueEncoding: 'json' });
+    log.append({ n: 3 });
+    log.append({ n: 4 });
+    log.append({ n: 100 });
+    
+    var n = 0;
+    log.on('add', function () {
+        if (++n !== 3) return;
+        var dex = indexer(log, idb, function (row, tx, next) {
+            tx.get('state', function (err, value) {
+                tx.put('state', (value || 0) + row.value.n, next);
+            });
+        });
+        var tx = dex.transaction();
+        tx.get('state', function (err, value) {
+            t.ifError(err);
+            t.equal(value, 107);
+            tx.close();
+        });
+    });
+});
