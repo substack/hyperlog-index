f286bb25c26786d3c2d14749c4652e0b00e8baa3
James Halliday <mail@substack.net>
2015-07-09 21:28:38 -0700

    add test

---
+++ test/add.js
@@ -0,0 +1,37 @@
+var memdb = require('memdb');
+var indexer = require('../');
+var hyperlog = require('hyperlog');
+var test = require('tape');
+
+test('add', function (t) {
+    t.plan(5);
+    var hdb = memdb();
+    var idb = memdb({ valueEncoding: 'json' });
+    
+    var log = hyperlog(hdb, { valueEncoding: 'json' });
+    
+    var dex = indexer(log, idb, function (row, tx, next) {
+        tx.get('state', function (err, value) {
+            tx.put('state', (value || 0) + row.value.n, next);
+        });
+    });
+    log.add(null, { n:3 }, function (err, node0) {
+        t.ifError(err);
+        log.add([node0.key], { n: 4 }, function (err, node1) {
+            t.ifError(err);
+            log.add([node1.key], { n: 100 }, function (err, node2) {
+                t.ifError(err);
+                ready();
+            });
+        });
+    });
+    
+    function ready () {
+        var tx = dex.transaction();
+        tx.get('state', function (err, value) {
+            t.ifError(err);
+            t.equal(value, 107);
+            tx.close();
+        });
+    }
+});
