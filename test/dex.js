c2b1a321695e4dfdaaf610b8d08c3bdd85a31569
James Halliday <mail@substack.net>
2015-07-24 19:11:37 -0700

    updated dex test

--- test/dex.js
+++ test/dex.js
@@ -4,29 +4,32 @@ var hyperlog = require('hyperlog');
 var test = require('tape');
 
 test('from scratch indexes', function (t) {
-    t.plan(2);
-    var hdb = memdb();
-    var idb = memdb({ valueEncoding: 'json' });
-    
-    var log = hyperlog(hdb, { valueEncoding: 'json' });
-    
-    var dex = indexer(log, idb, function (row, tx, next) {
-        tx.get('state', function (err, value) {
-            tx.put('state', (value || 0) + row.value.n, next);
-        });
+  t.plan(3);
+  var hdb = memdb();
+  var idb = memdb({ valueEncoding: 'json' });
+  
+  var log = hyperlog(hdb, { valueEncoding: 'json' });
+  
+  var dex = indexer(log, idb, function (row, tx, next) {
+    tx.get('state', function (err, value) {
+      tx.put('state', (value || 0) + row.value.n, next);
     });
-    log.append({ n: 3 });
-    log.append({ n: 4 });
-    log.append({ n: 100 });
-    
-    var n = 0;
-    log.on('add', function () {
-        if (++n !== 3) return;
-        var tx = dex.transaction();
-        tx.get('state', function (err, value) {
-            t.ifError(err);
-            t.equal(value, 107);
-            tx.close();
-        });
+  });
+  log.append({ n: 3 });
+  log.append({ n: 4 });
+  log.append({ n: 100 });
+  
+  var n = 0;
+  log.on('add', function () {
+    if (++n !== 3) return;
+    log.heads(function (err, heads) {
+      t.ifError(err);
+      var tx = dex.transaction(heads[0].key);
+      tx.get('state', function (err, value) {
+        t.ifError(err);
+        t.equal(value, 107);
+        tx.close();
+      });
     });
+  });
 });
