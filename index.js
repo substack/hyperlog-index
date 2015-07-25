ee9538ec42276fc2bfee9a08ca2fa5de8a94c190
James Halliday <mail@substack.net>
2015-07-24 19:07:37 -0700

    new implementation with level-forks works, passes the add test

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var inherits = require('inherits');
 var EventEmitter = require('events').EventEmitter;
 var Deferred = require('deferred-leveldown');
 var levelup = require('levelup');
+var xtend = require('xtend');
 
 module.exports = Ix;
 inherits(Ix, EventEmitter);
@@ -13,12 +14,17 @@ function Ix (log, db, fn) {
   if (!(this instanceof Ix)) return new Ix(log, db, fn);
   EventEmitter.call(this);
   var self = this;
-  this.forks = Forks(db);
-  this._ready = false;
+  this.forks = Forks(db, db.options);
+  this._options = db.options;
+  this._state = 'stale';
+  this._pending = 0;
+  log.on('add', function () { self._pending ++ });
   var r = log.createReadStream();
   r.pipe(through.obj(write, end));
   
   function write (row, enc, next) {
+    var prevstate = self._state;
+    self._state = 'processing';
     self.forks.create(
       row.key,
       row.links,
@@ -30,27 +36,51 @@ function Ix (log, db, fn) {
       var tx = transaction(c, db.options);
       fn(row, tx, function (err) {
         if (err) next(err)
-        else tx.commit(next)
+        else tx.commit(function (err) {
+          if (err) return next(err)
+          self._change = row.change;
+          self._state = prevstate;
+          if (--self._pending === 0 && prevstate === 'live') {
+            self.emit('ready')
+          }
+          next();
+        })
       });
     }
   }
   function end () {
-    log.createReadStream({ live: true }).pipe(through.obj(write));
-    self._ready = true;
+    log.createReadStream({
+      live: true,
+      since: self._change
+    }).pipe(through.obj(write));
+    self._state = 'live';
     self.emit('ready');
   }
 }
 
-Ix.prototype.transaction = function (seq) {
+Ix.prototype.transaction = function (seq, opts) {
   var self = this;
-  if (!self._ready) {
-    self.once('ready', function () {
-      def.setDb(self.transaction(seq));
-    });
-    var def = new Deferred;
-    return levelup('fake', { db: function () { return def } });
-  }
-  var tx = transaction(self.forks.open(seq));
+  var def = new Deferred;
+  var up = levelup('fake', xtend(
+    xtend(self._options, opts),
+    { db: function () { return def } }
+  ));
+  var tx = transaction(up);
   tx.close = function () { tx.rollback() };
+  
+  self.ready(function () {
+    def.setDb(self.forks.open(seq));
+  });
   return tx;
+};
+
+Ix.prototype.ready = function (fn) {
+  var self = this;
+  if (self._state === 'live' && self._pending === 0) {
+    process.nextTick(function () {
+      if (self._state === 'live') fn()
+      else self.once('ready', fn)
+    });
+  }
+  else self.once('ready', fn)
 };
--- test/add.js
+++ test/add.js
@@ -4,34 +4,39 @@ var hyperlog = require('hyperlog');
 var test = require('tape');
 
 test('add', function (t) {
-    t.plan(5);
-    var hdb = memdb();
-    var idb = memdb({ valueEncoding: 'json' });
-    
-    var log = hyperlog(hdb, { valueEncoding: 'json' });
-    
-    var dex = indexer(log, idb, function (row, tx, next) {
-        tx.get('state', function (err, value) {
-            tx.put('state', (value || 0) + row.value.n, next);
-        });
+  t.plan(5);
+  var hdb = memdb();
+  var idb = memdb({ valueEncoding: 'json' });
+  var log = hyperlog(hdb, { valueEncoding: 'json' });
+  
+  var dex = indexer(log, idb, function (row, tx, next) {
+    tx.get('state', function (err, value) {
+      tx.put('state', (value || 0) + row.value.n, next);
     });
-    log.add(null, { n:3 }, function (err, node0) {
+  });
+  log.add(null, { n:3 }, function (err, node0) {
+    t.ifError(err);
+    log.add([node0.key], { n: 4 }, function (err, node1) {
+      t.ifError(err);
+      log.add([node1.key], { n: 100 }, function (err, node2) {
         t.ifError(err);
-        log.add([node0.key], { n: 4 }, function (err, node1) {
-            t.ifError(err);
-            log.add([node1.key], { n: 100 }, function (err, node2) {
-                t.ifError(err);
-                ready();
-            });
-        });
+        ready();
+      });
     });
+  });
     
     function ready () {
-        var tx = dex.transaction();
-        tx.get('state', function (err, value) {
-            t.ifError(err);
-            t.equal(value, 107);
-            tx.close();
-        });
+      log.heads(function (err, heads) {
+        heads.forEach(onhead);
+      });
+    }
+    
+    function onhead (head) {
+      var tx = dex.transaction(head.key);
+      tx.get('state', function (err, value) {
+        t.ifError(err);
+        t.equal(value, 107);
+        tx.close();
+      });
     }
 });
