9240de6bd9889a3d003ba255cf69738d4442b2df
James Halliday <mail@substack.net>
2015-12-02 15:19:14 -0600

    kv fork test

---
+++ test/kv_fork.js
@@ -0,0 +1,48 @@
+var memdb = require('memdb')
+var indexer = require('../')
+var hyperlog = require('hyperlog')
+var test = require('tape')
+
+var db = memdb({ valueEncoding: 'json' })
+var log = hyperlog(memdb(), { valueEncoding: 'json' })
+
+test('kv fork', function (t) {
+  t.plan(5)
+  var dex = indexer(log, db, function (row, next) {
+    db.get(row.value.k, function (err, doc) {
+      if (!doc) doc = {}
+      row.links.forEach(function (link) {
+        delete doc[link]
+      })
+      doc[row.key] = row.value.v
+      db.put(row.value.k, doc, next)
+    })
+  })
+
+  var nodes = []
+  log.add(null, { k: 'a', v: 3 }, function (err, node0) {
+    t.ifError(err)
+    nodes.push(node0)
+    log.add([node0.key], { k: 'a', v: 4 }, function (err, node1) {
+      t.ifError(err)
+      nodes.push(node1)
+      log.add([node1.key], { k: 'a', v: 8 }, function (err, node2) {
+        t.ifError(err)
+        nodes.push(node2)
+        log.add([node0.key], { k: 'a', v: 15 }, function (err, node3) {
+          t.ifError(err)
+          nodes.push(node3)
+        })
+      })
+    })
+  })
+
+  dex.ready(function () {
+    db.get('a', function (err, values) {
+      var expected = {}
+      expected[nodes[3].key] = 15
+      expected[nodes[2].key] = 8
+      t.deepEqual(values, expected, 'expected values')
+    })
+  })
+})
